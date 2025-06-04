938055de747cd51c909554e82e461cb409d14d0b
Max <smolinskijmaksim1@gmail.com>
2025-06-04 17:30:12 +0100

    Change url name

--- cinema/urls.py
+++ cinema/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
